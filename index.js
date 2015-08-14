fa7a6a349b85c1197b783f817702c2d683fcaffc
Joseph Frazier <joseph@onsip.com>
2015-08-14 04:03:36 -0400

    traverse/filter/set paths instead of forEach

--- index.js
+++ index.js
@@ -4,13 +4,10 @@ import isPromise from 'is-promise'
 export default deepResolve
 
 function deepResolve (obj) {
-  var promises = [];
-
-  traverse(obj).forEach(function (x) {
-    if (isPromise(x)) {
-      promises.push(x.then(this.update.bind(this)));
-    }
-  });
+  var t = traverse(obj);
+  var promises = t.paths()
+    .filter(path => isPromise(t.get(path)))
+    .map(path => t.get(path).then(v => t.set(path, v)))
 
   return Promise.all(promises).then(() => obj);
 }
