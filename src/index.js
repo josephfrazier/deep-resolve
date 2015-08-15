149143ab8758aa2de8012fd2dd263cd6f93047c0
Joseph Frazier <joseph@onsip.com>
2015-08-15 08:52:18 -0400

    Use const instead of var

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import traverse from 'traverse'
 import isPromise from 'is-promise'
 
 export default obj => {
-  var t = traverse(obj);
-  var promises = t.paths()
+  const t = traverse(obj);
+  const promises = t.paths()
     .map(path => ({path, node: t.get(path)}))
     .filter(({path, node}) => isPromise(node))
     .map(({path, node}) => node.then(value => t.set(path, value)))
--- test/index.js
+++ test/index.js
@@ -7,8 +7,8 @@ import deepResolve from '../'
 test('example from traverse readme', t => {
   t.plan(1);
 
-  var obj =      [ 5, 6,  -3, [ 7, 8,  -2, 1 ], { f: 10, g: -13 } ];
-  var expected = [ 5, 6, 125, [ 7, 8, 126, 1 ], { f: 10, g: 115 } ];
+  const obj =      [ 5, 6,  -3, [ 7, 8,  -2, 1 ], { f: 10, g: -13 } ];
+  const expected = [ 5, 6, 125, [ 7, 8, 126, 1 ], { f: 10, g: 115 } ];
 
   traverse(obj).forEach(function (x) {
       if (x < 0) this.update(Promise.resolve(x + 128));
