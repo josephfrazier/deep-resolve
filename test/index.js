05d5429a23c5eaa8ba51cf43b3e6e413c1d29195
Joseph Frazier <joseph@onsip.com>
2015-08-15 08:36:13 -0400

    test/index.js - use arrow functions

--- test/index.js
+++ test/index.js
@@ -4,7 +4,7 @@ import test from 'tape'
 import traverse from 'traverse'
 import deepResolve from '../'
 
-test('example from traverse readme', function (t) {
+test('example from traverse readme', t => {
   t.plan(1);
 
   var obj =      [ 5, 6,  -3, [ 7, 8,  -2, 1 ], { f: 10, g: -13 } ];
@@ -14,7 +14,5 @@ test('example from traverse readme', function (t) {
       if (x < 0) this.update(Promise.resolve(x + 128));
   });
 
-  deepResolve(obj).then(function (resolved) {
-    t.deepEqual(resolved, expected);
-  })
+  deepResolve(obj).then(resolved => t.deepEqual(resolved, expected))
 });
